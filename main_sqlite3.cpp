008116b6029b7e0c71535af34101c5278a43423d
lhb <lhb198710@gmail.com>
2015-04-04 19:02:01 +0800

    添加对数据库的操作

--- main_sqlite3.cpp
+++ main_sqlite3.cpp
@@ -6,6 +6,13 @@
 #define NAME_SQLITE3_DB	"test.db"
 
 static void main_sqlite3_test_open(const char *dbName);
+static int main_sqlite3_test_open_callback(void *data, int argc, char **argv, char **names);
+
+struct main_sqlite3_open_data
+{
+	int		hasPrintHeader;
+	int		row;
+};
 
 void main_sqlite3_test()
 {
@@ -16,18 +23,60 @@ void main_sqlite3_test_open(const char *name)
 {
 	sqlite3 *db;
 	int rc;
+	char *zErrMsg;
+	struct main_sqlite3_open_data data;
 
 	db = NULL;
 	if (rc = sqlite3_open(name, &db))
 	{
 		fprintf(stdout, "open database failure: %s reason=%s\n", name, sqlite3_errmsg(db));
-		if (db != NULL) {
-			sqlite3_close(db);
-			db = NULL;
-		}
-		return;
+		goto FINISH;
 	}
 
+	data.hasPrintHeader = 0;
+	data.row = 0;
+
+	rc = sqlite3_exec(db, "SELECT * FROM tbl_users", main_sqlite3_test_open_callback, (void*) &data,  &zErrMsg);
+	if (rc != SQLITE_OK)
+	{
+		fprintf(stderr, "SQL error:%s\n", zErrMsg);
+		goto FINISH;
+	}
+
+FINISH:
 	sqlite3_close(db);
 	db = NULL;
+}
+
+int main_sqlite3_test_open_callback(void *data, int argc, char **argv, char **names)
+{
+	struct main_sqlite3_open_data *ptr;
+	int i;
+
+	ptr = (struct main_sqlite3_open_data*) data;
+	if (!ptr->hasPrintHeader)
+	{
+		ptr->hasPrintHeader = 1;
+		for (i = 0; i < argc; ++i)
+		{
+			if(i != 0)
+			{
+				fprintf(stdout, "\t");
+			}
+			fprintf(stdout, "%s", names[i]);
+		}
+		fprintf(stdout, "\n");
+	}
+
+	for (i = 0; i < argc; ++i)
+	{
+		if (i != 0)
+		{
+			fprintf(stdout, "\t");
+		}
+		fprintf(stdout, "%s", argv[i] ? argv[i] : "NULL");
+	}
+	++ptr->row;
+
+	return 0;
 }
